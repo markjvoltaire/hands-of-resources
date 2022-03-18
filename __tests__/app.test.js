37076871d00eb66af24a413dce378365cc9773cb
Mark Voltaire <markjvoltaire@gmail.com>
2022-03-17 20:32:54 -0400

    controller conected to test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,1 +1,26 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
 
+describe('handOfResources routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('should create a hat', async () => {
+    const res = await request(app)
+      .post('/api/v1/hats')
+      .send({ kind: 'snapback', color: 'red' });
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      kind: 'snapback',
+      color: 'red',
+    });
+  });
+});
